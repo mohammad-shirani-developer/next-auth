0dc04bb5ab51a2e42cb9accdb8957101701105f6
Mohammad Shirani <mohammad.shirani.developer@gmail.com>
2025-09-02 15:00:07 +0330

    feat: Add signinAction in signinForm

--- src/app/(auth)/_components/sign-in-form.tsx
+++ src/app/(auth)/_components/sign-in-form.tsx
@@ -1,16 +1,15 @@
-'use client';
+"use client";
 
+import { signinAction } from "@/app/_actions/auth-actions";
+import Eye from "@/app/_assets/eye";
+import Phone from "@/app/_assets/phone";
+import { Button } from "@/app/_components/button";
+import { TextBox } from "@/app/_components/textbox";
+import { valibotResolver } from "@hookform/resolvers/valibot";
 import { FC, useTransition } from "react";
 import { useForm } from "react-hook-form";
-import { valibotResolver } from "@hookform/resolvers/valibot";
-import { SignInModel } from "../_types/auth.types";
 import { SignInSchema } from "../_types/auth.schema";
-import { TextBox } from "@/app/_components/textbox";
-import { Button } from "@/app/_components/button";
-import Phone from "@/app/_assets/phone";
-import Eye from "@/app/_assets/eye";
-
-
+import { SignInModel } from "../_types/auth.types";
 
 export const SignInForm: FC = () => {
   const {
@@ -23,21 +22,41 @@ export const SignInForm: FC = () => {
 
   const [isPending, startTransition] = useTransition();
 
-
   const onSubmit = async (data: SignInModel) => {
     startTransition(async () => {
-      // const response = await signInAction(data);
-      console.log(data);
+      const response = await signinAction(data);
+      console.log(response);
     });
   };
   return (
     <div className=" w-full">
       <form
         onSubmit={handleSubmit(onSubmit)}
-        className='flex flex-col gap-6 mt-6'>
-        <TextBox name={'username'} register={register} errors={errors} type="number" placeholder="شماره موبایل" label="شماره موبایلت رو وارد کن" icon={<Phone />} />
-        <TextBox name={'password'} register={register} errors={errors} type="number" placeholder="رمز عبور" label="رمز عبورت رو وارد کن" icon={<Eye />} />
-        <Button type="submit" loading={isPending} className="w-full">ورود به پلتفرم</Button>
+        className="flex flex-col gap-6 mt-6"
+      >
+        <TextBox
+          name={"username"}
+          register={register}
+          errors={errors}
+          type="number"
+          placeholder="شماره موبایل"
+          label="شماره موبایلت رو وارد کن"
+          value="09136007466"
+          icon={<Phone />}
+        />
+        <TextBox
+          name={"password"}
+          register={register}
+          errors={errors}
+          type="password"
+          placeholder="رمز عبور"
+          label="رمز عبورت رو وارد کن"
+          value="123456789"
+          icon={<Eye />}
+        />
+        <Button type="submit" loading={isPending} className="w-full">
+          ورود به پلتفرم
+        </Button>
       </form>
     </div>
   );
